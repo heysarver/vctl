abf92b3b39eb0b4352e1c8417f1308d2a8f89aef
astromodder <22250203+astromodder@users.noreply.github.com>
2023-12-22 14:02:45 -0500

    test

--- vctl.sh
+++ vctl.sh
@@ -6,12 +6,25 @@ parameters=("$@")
 
 case "$action" in
   create)
+    if [ -z "${VC_DOMAIN}" ]; then
+      echo "Error: VC_DOMAIN environment variable is not set.  See README.md."
+      exit 1
+    fi
     echo "Creating the vcluster..."
     ./scripts/create.sh $parameters
     ;;
   delete)
-    echo "Deleting the vcluster..."
-    ./scripts/delete.sh $parameters
+    echo "Are you sure you want to delete the vcluster? (y/n)"
+    read confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+      echo "Deleting the vcluster..."
+      ./scripts/delete.sh $parameters
+    else
+      echo "Aborted deleting the vcluster."
+    fi
+    ;;
+  list)
+    vcluster list $parameters
     ;;
   *)
     echo "Command not found"
